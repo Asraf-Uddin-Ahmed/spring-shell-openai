6488e1bf9bcc4b4d78c690705d4c51547fbcb75e
Asraf Uddin Ahmed <13ratul@gmail.com>
2023-07-21 10:40:28 +0200

    Create basic Spring Shell project using Spring Initializr

---
+++ src/main/java/com/asraf/springshellopenai/SpringShellOpenaiApplication.java
@@ -0,0 +1,13 @@
+package com.asraf.springshellopenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShellOpenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShellOpenaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asraf/springshellopenai/SpringShellOpenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asraf.springshellopenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShellOpenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
